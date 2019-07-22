5b91837ab69278b8675086873a64c08e994fc619
EugeneRus <neoanon2001@gmail.com>
2019-07-22 23:19:36 +0700

    drop dead code

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -6,30 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 
 class MainActivity : AppCompatActivity() {
 
-//    lateinit var benderImage: ImageView
-//   lateinit var textTxt: TextView
-//    lateinit var messageEt: EditText
-//    lateinit var sendBtn: ImageView
-
     lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        benderImage = iv_bender
-//        textTxt = tv_text
-//        messageEt = et_message
-//        sendBtn = iv_send
 
         val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
         val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
